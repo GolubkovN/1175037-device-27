ca6b5e6c152626f54c819b347fd4792748eda3f9
Golubkov Nikita <57894854+GolubkovN@users.noreply.github.com>
2020-02-28 17:19:25 +0200

    Реализовал интерактивные элементы

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,31 @@
 var slider = function () {
-    var sliderNav = document.querySelectorAll(".points__btn");
+    var sliderNav = document.querySelectorAll(".dots__btn");
     var sliderContent = document.querySelectorAll(".hero-slider__item");
     var sliderName;
 
-    sliderNav.forEach(function (item) {
-        item.addEventListener("click", selectSliderNav)
-        
-    });
+    for(var i = 0; i < sliderNav.length; i++) {
+        sliderNav[i].addEventListener("click", selectSliderNav);
+    };
 
-    function selectSliderNav() {
-        sliderNav.forEach(function (item) {
-            item.classList.remove("points__btn--active");
-        });
-        this.classList.add("points__btn--active");
+    function selectSliderNav(evt) {
+        evt.preventDefault();
+        for (var i = 0; i < sliderNav.length; i++) {
+            sliderNav[i].classList.remove("dots__btn--active");
+        }
+        this.classList.add("dots__btn--active");
         sliderName = this.getAttribute("data-name");
         selectSliderContent(sliderName);
-        console.log("click");
     }
 
     function selectSliderContent(sliderName) {
-        sliderContent.forEach(function (item) {
-            item.classList.contains(sliderName) ? item.classList.add("hero-slider__item--visual") : 
-            item.classList.remove("hero-slider__item--visual");
-        })
+       for (var j = 0; j < sliderContent.length; j++) {
+           if (sliderContent[j].classList.contains(sliderName)) {
+            sliderContent[j].classList.add("hero-slider__item--visual");
+           }else {
+            sliderContent[j].classList.remove("hero-slider__item--visual");
+           }
+        }
     }
 };
 slider();
+
--- js/tab.js
+++ js/tab.js
@@ -3,26 +3,30 @@ var tab = function () {
     var tabContent = document.querySelectorAll(".services__item");
     var tabName;
 
-    tabNav.forEach(function (item) {
-        item.addEventListener("click", selectTabNav)
-    });
+    for(var i = 0; i < tabNav.length; i++) {
+        tabNav[i].addEventListener("click", selectTabNav);
+    };
 
     function selectTabNav(evt) {
         evt.preventDefault();
-        tabNav.forEach(function (item) {
-            item.classList.remove("is-active");
-        });
+        for(var i = 0; i < tabNav.length; i++) {
+            tabNav[i].classList.remove("is-active");
+        }
         this.classList.add("is-active");
         tabName = this.getAttribute("data-filter");
         selectTabContent(tabName);
     }
 
     function selectTabContent(tabName) {
-        tabContent.forEach(function (item) {
-            item.classList.contains(tabName) ? item.classList.add("services__item--active") :
-             item.classList.remove("services__item--active");
-        })
+        for(var j = 0; j < tabContent.length; j++) {
+            if (tabContent[j].classList.contains(tabName)) {
+                tabContent[j].classList.add("services__item--active");
+            }else {
+                tabContent[j].classList.remove("services__item--active");
+            }
+        }
     }
 };
 
 tab();
+            
